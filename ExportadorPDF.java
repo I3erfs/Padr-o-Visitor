9447135ea0e471af153214dac40ee0e003207367
Bernardo Friske <148400713+I3erfs@users.noreply.github.com>
2026-07-01 21:37:08 -0300

    Implement PDF and XML export functionality

---
+++ ExportadorPDF.java
@@ -0,0 +1,21 @@
+// Implementação para PDF
+public class ExportadorPDF implements VisitorExportacao {
+    public void visitarRelatorioA(RelatorioA relatorio) {
+        System.out.println("Exportando " + relatorio.getDados() + " para PDF.");
+    }
+
+    public void visitarRelatorioB(RelatorioB relatorio) {
+        System.out.println("Exportando " + relatorio.getOutrosDados() + " para PDF.");
+    }
+}
+
+// Implementação para XML
+public class ExportadorXML implements VisitorExportacao {
+    public void visitarRelatorioA(RelatorioA relatorio) {
+        System.out.println("Exportando " + relatorio.getDados() + " para XML.");
+    }
+
+    public void visitarRelatorioB(RelatorioB relatorio) {
+        System.out.println("Exportando " + relatorio.getOutrosDados() + " para XML.");
+    }
+}
